319f71246d2322702ae68e559a4b761e2616f616
Annie Y <annie_y48@hotmail.com>
2017-01-07 18:18:31 -0800

    Initial commit

---
+++ public/app.jsx
@@ -0,0 +1,49 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  getInitialState: function () {
+    return {
+        name: this.props.name
+    };
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Annie';
+
+ReactDOM.render(
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
