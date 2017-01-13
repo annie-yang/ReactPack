31bad2ed6a37f887b1abf92de5c8256568340c4e
Annie Y <annie_y48@hotmail.com>
2017-01-13 14:34:16 -0800

    Add automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+// formats output to be more readable
+describe('App', () => {
+  // describes what the function should do
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
