bd99a074ae8b501c60909f81469fb3e4184a9ce9
Annie Y <annie_y48@hotmail.com>
2017-01-19 09:57:27 -0800

    Fixed switching tabs for navigation

--- app/app.jsx
+++ app/app.jsx
@@ -1,21 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
 var Main = require('Main');
+var Greeter = require('Greeter');
+var Weather = require('Weather');
 var Timer = require('Timer');
 var Countdown = require('Countdown');
 
-// Load foundation
 $(document).foundation();
 
-// App css
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="countdown" component={Countdown}/>
-      <IndexRoute component={Timer}/>
+      <Route path="Weather" component={Weather}/>
+      <Route path="Timer" component={Timer}/>
+      <Route path="Countdown" component={Countdown}/>
+      <IndexRoute component={Greeter}/>
     </Route>
   </Router>,
   document.getElementById('app')
