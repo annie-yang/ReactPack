aadcecd156181da6f5ff75f6f79ed0fa4dbdddfd
Annie Y <annie_y48@hotmail.com>
2017-01-23 11:05:52 -0800

    Added filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -26,6 +26,21 @@ module.exports = {
       return !todo.completed || showCompleted;
     });
 
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+    
+    filteredTodos.sort((a, b) => {
+      if(!a.completed === false && b.completed){
+        return -1;
+      } else if(a.completed && !b.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
     return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -77,5 +77,20 @@ describe('TodoAPI', () => {
   		var filteredTodos = TodoAPI.filterTodos(todos, false, '');
   		expect(filteredTodos.length).toBe(1);
   	 });
+
+     it('should sort by completed status', () => {
+       var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+       expect(filteredTodos[0].completed).toBe(false);
+     });
+
+     it('should filter todos by searchText', () => {
+       var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+       expect(filteredTodos.length).toBe(2);
+     });
+
+     it('should return all todos if searchText is empty', () => {
+       var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+       expect(filteredTodos.length).toBe(3);
+     });
   });
 });
